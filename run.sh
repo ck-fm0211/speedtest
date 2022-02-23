bc82f95402afd1e13dc5313ae17578506a833354
ck-fm0211 <chikafumi.k@gmail.com>
2022-02-23 17:04:31 +0900

    feat: swich server if open project does not exist

--- run.sh
+++ run.sh
@@ -23,14 +23,21 @@ echo ${NOW}
 # 結果ファイル
 RESULT_FILENAME_JSON=result_${MACHINE}_${ISP}_${NETWORK_INTERFACE_ID}_${NOW}.json
 RESULT_FILENAME_TSV=result_${MACHINE}_${ISP}_${NETWORK_INTERFACE_ID}_${NOW}.tsv
+SERVER_ID=server_id_${MACHINE}_${ISP}_${NETWORK_INTERFACE_ID}_${NOW}.txt
 
 # speedtestの結果を出力
 echo "== exec speedtest =="
-speedtest -L | grep "OPEN Project" | awk '{print $1}' | xargs -I{} speedtest -f json -s {} -I ${NETWORK_INTERFACE_ID}  > output/${RESULT_FILENAME_JSON}
+speedtest -L | grep "OPEN Project" | awk '{print $1}' > output/${SERVER_ID}
+if [ ! -s $FILE ]; then
+    #OPEN Projectが存在しない場合はサーバーを指定しない
+    speedtest -f json -I ${NETWORK_INTERFACE_ID}  > output/${RESULT_FILENAME_JSON}
+else
+    cat output/${SERVER_ID} | xargs -I{} speedtest -f json -s {} -I ${NETWORK_INTERFACE_ID}  > output/${RESULT_FILENAME_JSON}
+fi
 
 echo "== parse result =="
 cat output/${RESULT_FILENAME_JSON} \
-| jq -r '. | [(.timestamp | strptime("%Y-%m-%dT%H:%M:%SZ") | mktime + (60 * 60 * 9) | strftime("%F %X")), .ping.jitter, .ping.latency, .download.bandwidth, .download.bytes, .download.elapsed, .upload.bandwidth, .upload.bytes, .upload.elapsed, .packetLoss] | @tsv' \
+| jq -r '. | [(.timestamp | strptime("%Y-%m-%dT%H:%M:%SZ") | mktime + (60 * 60 * 9) | strftime("%F %X")), .ping.jitter, .ping.latency, .download.bandwidth, .download.bytes, .download.elapsed, .upload.bandwidth, .upload.bytes, .upload.elapsed, .packetLoss, .server.name] | @tsv' \
 | awk -v m=${MACHINE} -v i=${ISP} -v n=$NETWORK_INTERFACE_NAME -F "\t" 'BEGIN{OFS="\t"}{$NF=$NF"\t"m"\t"i"\t"n;print}' \
 > output/${RESULT_FILENAME_TSV}
 
@@ -40,5 +47,6 @@ python upload.py ${RESULT_FILENAME_TSV}
 echo "==rm files=="
 rm -f output/${RESULT_FILENAME_JSON}
 rm -f output/${RESULT_FILENAME_TSV}
+rm -f output/${SERVER_ID}
 
 echo "== end =="
